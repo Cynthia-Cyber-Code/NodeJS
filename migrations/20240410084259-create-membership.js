19aa497e5d17907090d464866039a2fdcf1d7b4b
Cynthia-Cyber-Code <cynthiaf181999@gmail.com>
2024-06-13 15:28:08 +0200

    fix migration CICD

--- migrations/20240410084259-create-membership.js
+++ migrations/20240410084259-create-membership.js
@@ -18,6 +18,9 @@ module.exports = {
       expiration_date: {
         type: Sequelize.DATE
       },
+      userId: {
+        type: Sequelize.INTEGER,
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
