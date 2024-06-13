0efb89cb491406ae9ab93131b95136ded25f2f9c
Cynthia-Cyber-Code <cynthiaf181999@gmail.com>
2024-06-13 18:18:44 +0200

    swagger reservation

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,53 @@ router.get("/", (req, res) => {
   res.json({ message: "Launch page" });
 });
 
+/**
+ * @swagger
+ * /api/reservations:
+ *   get:
+ *     summary: Récupérer une reservation
+ *     description: Récupère toute les reservations d'un utilisateur dans la base de données
+ *     responses:
+ *        200:
+ *         description: Reservations récupéré avec succès
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                    type: string
+ *                    example: "Vos réservations"
+ *   post:
+ *     summary: Effectuer une reservation
+ *     description: Ajoute une reservation dans la base de données
+ *     responses:
+ *       200:
+ *         description: Reservation effectuée avec succès
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Votre reservation a bien été enregistrée"
+ *   put:
+ *     summary: Modifie une reservation
+ *     description: Modifie une reservation dans la base de données
+ *     responses:
+ *       200:
+ *         description: Reservation modifie avec succès
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Votre reservation a été modifiée avec succès"
+ */
+
 router.use("/auth", authRouter);
 
 router.use("/users", verifyToken, usersRouter);
